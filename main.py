19ba88a2dca07064564295db84e8e226a039b5bf
Gulaal <egorveschev315@gmail.com>
2026-05-12 22:01:42 +0300

    gauss 4

--- main.py
+++ main.py
@@ -24,12 +24,11 @@ def simpson(g, a, b, eps, max_iter):
         S2 += 2 * np.sum(gs2[2:-1:2])
         S2 *= h2 / 3
 
-        print(S2, S,)
         if abs(S2 - S) < eps:
-            return S2
+            return S2, N2
         N = N2
 
-    return S2
+    return S2, N
 
 def gauss_3_nodes(g, a, b, n=4):
     xi =  np.array([-np.sqrt(3/5), 0.0, np.sqrt(3/5)])
@@ -44,6 +43,20 @@ def gauss_3_nodes(g, a, b, n=4):
         S += (h / 2) * np.sum(w * (g(x_nodes)))
     return S
 
+def gauss_4_nodes(g, a, b, n=4):
+    xi = np.array([-0.8611363, -0.3399810, 0.3399810, 0.8611363])
+    w = np.array([0.3478548, 0.6521451, 0.6521451, 0.3478548])
+
+    h = (b - a) / n
+    S = 0
+    for i in range(n):
+        x_left = a + i * h
+        x_mid = x_left + h / 2
+        x_nodes = x_mid + (h / 2) * xi
+        S += (h / 2) * np.sum(w * (g(x_nodes)))
+    return S
+
+
 if __name__ == "__main__":
     a = -1
     b = 0
@@ -54,10 +67,14 @@ if __name__ == "__main__":
     tau = (d - c) / m
     ts = [c + j * tau for j in range(m + 1)]
 
+    print("t        Simpson  N     Gauss3      Gauss4")
     for t in ts:
         g = lambda x: f(t, x)
-        I_Simpson = simpson(g, a, b, eps, 20)
-        I_Gauss = gauss_3_nodes(g, a, b)
-        print(f"Симпсон {I_Simpson} для t={t}")
-        print(f"Гаусс {I_Gauss} для t={t}")
+        I_Simpson, N = simpson(g, a, b, eps, 20)
+        I_Gauss3 = gauss_3_nodes(g, a, b)
+        I_Gauss4 = gauss_4_nodes(g, a, b)
+
+        print(f"{t:.2f}     {I_Simpson:.5f}  {N}    {I_Gauss3:.5f}     {I_Gauss4:.5f}")
+        # print(f"Симпсон {I_Simpson:.5f} для t={t:.3f}")
+        # print(f"Гаусс {I_Gauss:.5f} для t={t:.3f}\n")
         
